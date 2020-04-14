96de610de89789d856e2ad971dd20c04b635a695
Sebastian Delima <sebastiandelimagec@gmail.com>
2020-04-14 15:48:02 -0400

    finished

--- email/app/controllers/visiteds_controller.rb
+++ email/app/controllers/visiteds_controller.rb
@@ -1,74 +1,22 @@
 class VisitedsController < ApplicationController
   before_action :set_visited, only: [:show, :edit, :update, :destroy]
 
-  # GET /visiteds
-  # GET /visiteds.json
   def index
     visits = Visited.all
     render :json => visits
   end
 
-  # GET /visiteds/1
-  # GET /visiteds/1.json
-  def show
-  end
-
-  # GET /visiteds/new
-  def new
-    @visited = Visited.new
-  end
-
-  # GET /visiteds/1/edit
-  def edit
-  end
-
-  # POST /visiteds
-  # POST /visiteds.json
   def create
-    @visited = Visited.new(visited_params)
-
-    respond_to do |format|
-      if @visited.save
-        format.html { redirect_to @visited, notice: 'Visited was successfully created.' }
-        format.json { render :show, status: :created, location: @visited }
-      else
-        format.html { render :new }
-        format.json { render json: @visited.errors, status: :unprocessable_entity }
-      end
-    end
+  
+    visit = Visited.create(visited_params)
+    visits = Visited.all
+    render :json => visits
   end
 
-  # PATCH/PUT /visiteds/1
-  # PATCH/PUT /visiteds/1.json
-  def update
-    respond_to do |format|
-      if @visited.update(visited_params)
-        format.html { redirect_to @visited, notice: 'Visited was successfully updated.' }
-        format.json { render :show, status: :ok, location: @visited }
-      else
-        format.html { render :edit }
-        format.json { render json: @visited.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
-  # DELETE /visiteds/1
-  # DELETE /visiteds/1.json
-  def destroy
-    @visited.destroy
-    respond_to do |format|
-      format.html { redirect_to visiteds_url, notice: 'Visited was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_visited
-      @visited = Visited.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
+  
     def visited_params
       params.require(:visited).permit(:times)
     end
